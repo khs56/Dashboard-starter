1570d8eb193deb98a091738f629f810b9f940332
Vishal M <48333506+khs56@users.noreply.github.com>
2024-05-13 16:13:11 -0400

    Update Data.js

--- src/Data/Data.js
+++ src/Data/Data.js
@@ -6,7 +6,12 @@ import {
   UilPackage,
   UilChart,
   UilSignOutAlt,
+  UilUsdSquare,
+  MoneyWithdrawal
 } from "@iconscout/react-unicons";
+
+import { PiHandWithdrawLight } from "react-icons/pi";
+
 
 // sidebar data
 export const SidebarData = [
@@ -30,4 +35,55 @@ export const SidebarData = [
     icon: UilChart,
     heading: "Analytics",
   }
+];
+
+export const CardsData = [
+  {
+    title: "Sales",
+    color: {
+      backGround: "linear-gradient(180deg, #bb67ff 0%, #c484f3 100%)",
+      boxShadow: "0px 10px 20px 0px #e0c6f5",
+    },
+    barValue: 70,
+    value: "25,970",
+    png: UilUsdSquare,
+    series: [
+      {
+        name: "Sales",
+        data: [31, 40, 28, 51, 42, 109, 100],
+      }
+    ]
+  },
+  {
+    title: "Revenue",
+    color: {
+      backGround: "linear-gradient(180deg, #ff919d 0%, #fc929d 100%)",
+      boxShadow: "0px 10px 20px 0px #fdc0c7",
+    },
+    barValue: 80,
+    value: "14,270",
+    png: UilUsdSquare,
+    series: [
+      {
+        name: "Revenue",
+        data: [10, 100, 50, 70, 80, 30, 40],
+      }
+    ]
+  },
+  {
+    title: "Expenses",
+    color: {
+      backGround: "linear-gradient(180deg, #bb67ff 0%, #c484f3 100%)",
+      boxShadow: "0px 10px 20px 0px #e0c6f5",
+    },
+    barValue: 60,
+    value: "4,270",
+    png: UilClipboardAlt,
+    series: [
+      {
+        name: "Expenses",
+        data: [10, 25, 15, 30, 12, 15, 20],
+      }
+    ]
+  },
 ];
