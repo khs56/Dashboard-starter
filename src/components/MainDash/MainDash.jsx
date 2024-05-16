9dc271f43c8aa65306f2ea104825d63c7ff9ff22
Vishal M <48333506+khs56@users.noreply.github.com>
2024-05-16 11:58:32 -0400

    Update MainDash.jsx

--- src/components/MainDash/MainDash.jsx
+++ src/components/MainDash/MainDash.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Cards from '../Cards/Cards'
 import './MainDash.css'
 import Table from '../../components/Table/Table'
+import RightSide from '../RightSide/RightSide'
 
 const MainDash = () => {
   return (
@@ -9,6 +10,7 @@ const MainDash = () => {
       <h1>Dashboard</h1>
       <Cards />
       <Table />
+      <RightSide />
     </div>
   )
 }
