3d6a71deb661c0822ace2793904b96297b9e8295
Vishal M <48333506+khs56@users.noreply.github.com>
2024-05-16 11:20:04 -0400

    Create RightSide.jsx

---
+++ src/components/RightSide/RightSide.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './RightSide.css'
+
+const RightSide = () => {
+  return (
+    <div>
+      <div>
+        <h3>Updates</h3>
+        {/* <Updates /> */}
+      </div>
+      <div>
+        <h3>Customer Review</h3>
+      </div>
+    </div>
+  )
+}
+
+export default RightSide
